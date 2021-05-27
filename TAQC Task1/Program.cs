125360a318daf823b7d9dd7f032620fdb04f22a6
Nikita Semenov <semenovnikita2013@gmail.com>
2021-05-27 15:14:14 +0300

    Добавьте файлы проекта.

---
+++ TAQC Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TAQC_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-Check value 2-How are you");
+            int checkV = Convert.ToInt32(Console.ReadLine());
+            switch (checkV)
+            {
+                case 1:
+                    int a, b;
+                    Console.WriteLine("Enter a:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter b:");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Result of a + b = {a + b}");
+                    Console.WriteLine($"Result of a - b = { a - b}");
+                    Console.WriteLine($"Result of a * b = {a * b}");
+                    Console.WriteLine($"Result of a / b = {a / b}");
+
+                    break;
+                case 2:
+                    Console.WriteLine("How are you?");
+                    string answer = Console.ReadLine();
+                    Console.WriteLine($"You are {answer}");
+                    break;
+            }
+        }
+    }
+}
